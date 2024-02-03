f896011174025e6619395d286e4abc06169be793
swarit <43305013+swarit-pandey@users.noreply.github.com>
2024-02-03 08:38:46 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -3,33 +3,35 @@ import requests
 # Meme API endpoint
 API_URL = "https://meme-api.com/gimme"
 
-
 def fetch_random_meme():
     response = requests.get(API_URL)
     if response.status_code == 200:
-        meme_url = response.json().get('url')  # The URL of the meme image
-        return meme_url
+        data = response.json()
+        if data.get('nsfw'):
+            print("NSFW meme skipped")
+            return "skill-issue.gif", False 
+        else:
+            meme_url = data.get('url')  
+            return meme_url, True
     else:
         print("skill-issue :", response.status_code)
-        return None
-
-
-def update_readme(meme_url):
+        return "skill-issue.gif", False  
+        
+def update_readme(meme_url, is_direct_url):
+    if is_direct_url:
+        meme_markdown = f"![Random Meme]({meme_url})"
+    else:
+        meme_markdown = f"![Skill Issue]({meme_url})"
+    
     readme_contents = f"""
-# My GitHub Profile
+enjoy a random meme from reddit (updated by gh bot)
 
-Welcome to my GitHub profile! Enjoy a random meme every day:
-
-![Random Meme]({meme_url})
-
-Feel free to check out my projects below.
+{meme_markdown}
 """
 
     with open('README.md', 'w') as readme_file:
         readme_file.write(readme_contents)
 
-
 if __name__ == "__main__":
-    meme_url = fetch_random_meme()
-    if meme_url:
-        update_readme(meme_url)
+    meme_url, is_direct_url = fetch_random_meme()
+    update_readme(meme_url, is_direct_url)
